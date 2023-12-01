15251cc05ce43d9f88b7381845b70f724a77bc04
Raed Alnan <alnanraed@gmail.com>
2023-12-01 15:43:53 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//label-error input-error || day-error month-error year-error
+const thisYear = new Date().getFullYear()
+const thisMonth = new Date().getMonth()
+const thisDay = new Date().getDate()
+const monthArray = [31,28,31,30,31,30,31,31,30,31,30,31]
+const btn = document.querySelector(".arrow")
+
+let birthYearE = document.querySelector(".iyear")
+let birthMonthE = document.querySelector(".imonth")
+let birthDayE = document.querySelector(".iday")
+
+ btn.addEventListener("click" , ()=>{
+ })
+
